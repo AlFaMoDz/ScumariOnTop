ed7c1b1a335b11636d91ca0b527dff4420b61e99
alfa <ialfamodz@protonmail.com>
2019-12-28 20:21:06 +0100

    Final Project Fix. Added also travis support.

---
+++ Register.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Scumari
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VIDbox = new System.Windows.Forms.TextBox();
+            this.SECTORbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡Bienvenido a ScumariOnTop!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(292, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Antes de empezar a controlar deberás rellenar los siguientes \r\ndatos. Después pod" +
+    "rás empezar tu sesión =)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Introduce tu VID:";
+            // 
+            // VIDbox
+            // 
+            this.VIDbox.Location = new System.Drawing.Point(15, 117);
+            this.VIDbox.MaxLength = 6;
+            this.VIDbox.Name = "VIDbox";
+            this.VIDbox.Size = new System.Drawing.Size(100, 20);
+            this.VIDbox.TabIndex = 3;
+            // 
+            // SECTORbox
+            // 
+            this.SECTORbox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.SECTORbox.Location = new System.Drawing.Point(15, 179);
+            this.SECTORbox.MaxLength = 4;
+            this.SECTORbox.Name = "SECTORbox";
+            this.SECTORbox.Size = new System.Drawing.Size(100, 20);
+            this.SECTORbox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Introduce el sector en el que controlarás \r\n(LECM,LECB,LECS,GCCC):";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Empezar a controlar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label5.Location = new System.Drawing.Point(0, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(308, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Made with love by Álex (605126) from Spain";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 253);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SECTORbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.VIDbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "¡Bienvenido!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox VIDbox;
+        private System.Windows.Forms.TextBox SECTORbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
